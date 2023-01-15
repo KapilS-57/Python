1922afb3fa06af9cbcbbde2d10eeb6c307c58a7b
Kapil Sharma <capishr57@gmail.com>
2023-01-15 23:54:37 +0530

    Initial Commit

---
+++ SwapNums.py
@@ -0,0 +1,28 @@
+a = 10
+b = 15
+# c = 0
+
+# c = b #15
+# b = a #10
+# a = c #15
+
+# (a,b) = (b,a)
+
+# sum = a+b
+# a = sum-b
+# b = sum-a
+
+a = a+b #25
+b = a-b #10
+a = a-b #15
+
+# Q. 5L vessel and 3L vessel you have unlimited storage
+#    measure 4L?
+# A. Connect the dots
+
+
+# 1 inch = 25.4 mm
+
+print(a)
+print(b)
+
---
+++ Var.py
@@ -0,0 +1,44 @@
+a = 90
+print(a)
+
+b = 30
+sum = a + b
+
+print(sum)
+
+b = a
+sum = a + b
+print(b)
+
+print(sum)
+
+
+# Var Types
+# List , Set , Tuple , Dictionary
+
+# 1. List
+names = ["Kapil", "Jatin","Sumit"]
+print(names)
+
+print(type(names))
+print(names[-1])
+
+number = [1,2,3,4,5,6,7,8,9,11,16,164,134,245]
+
+print(type(number))
+print(number[3:7]) # Slicing
+
+names.append("Vishal") #Appending
+print(names)
+
+names.insert(1,"neha") # adding with position
+print(names)
+
+names.remove("neha") #removing 
+print(names)
+
+
+
+
+
+# 2. Set
